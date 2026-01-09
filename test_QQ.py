e7e496b77f9137bbdae3abd2a3220e2123487b31
QQTEST-0109 <625875899@qq.com>
2026-01-09 22:36:12 +0800

    testpipline

---
+++ test_QQ.py
@@ -0,0 +1,14 @@
+import os
+
+import pytest
+import allure_pytest
+
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_failure():
+    # 这个测试会失败，用于演示
+    assert 1 + 1 == 3
